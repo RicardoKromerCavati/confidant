ff671c9f0e3e383c77356d347ab70168419ce9e9
RicardoKCavati <ricardokcavati@gmail.com>
2025-05-21 20:46:47 -0300

    fix: Configure aliases correctly.

--- src/presentation/commands/credentials.ts
+++ src/presentation/commands/credentials.ts
@@ -3,7 +3,9 @@ import { CredentialService } from '../../domain/services/credentialService';
 
 export function asignCredentialCommands(program: Command) {
     program
-        .command('create-credential|create|cc')
+        .command('create')
+        .alias('cc')
+        .alias('create-credential')
         .description('Create new credential')
         .argument('<credentialName>', 'Credential name (you should chose a name you can remember to find the credential easily)')
         .argument('<username>', 'Username')
