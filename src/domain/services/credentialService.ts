2b7b61ef62a6c86c445cd9abd35b2c19cf06b587
RicardoKCavati <ricardokcavati@gmail.com>
2025-05-21 22:29:43 -0300

    chore: Create new method on service and use repository.

--- src/domain/services/credentialService.ts
+++ src/domain/services/credentialService.ts
@@ -1,4 +1,6 @@
-import { Credential } from '../../domain/models/credential'
+import { Credential } from '../../domain/models/credential';
+import { CredentialRepository } from '../../infrastructure/repositories/credentialRepository';
+
 export class CredentialService {
     public static createCredential(credentialName: string, username: string, password: string): void {
         try {
@@ -6,10 +8,32 @@ export class CredentialService {
 
             if (result && credential != null) {
                 console.log(`[FROM SERVICE]\nCredential name: ${credential.credentialName}\nUsername: ${credential.username}\nPassword: ${credential.password}`);
+
+                new CredentialRepository().createCredential(credential);
+
                 return;
             }
 
             console.log('Credential not created');
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    public static async getCredentials() {
+        try {
+            const foundCredentials = await new CredentialRepository().getCredentialNames();
+
+            if (foundCredentials.length <= 0) {
+                console.log('no credentials found');
+                return;
+            }
+
+            for (let index = 0; index < foundCredentials.length; index++) {
+                const element = foundCredentials[index];
+                console.log(element.credentialName);
+            }
 
         } catch (error) {
             console.log(error);
