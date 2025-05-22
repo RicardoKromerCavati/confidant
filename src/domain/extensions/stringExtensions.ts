35e6d489296df9bdb99e3b1aebb84614ccf0dbaa
RicardoKCavati <ricardokcavati@gmail.com>
2025-05-21 21:30:47 -0300

    chore: Create new string extensions.

--- src/domain/extensions/stringExtensions.ts
+++ src/domain/extensions/stringExtensions.ts
@@ -1,11 +1,22 @@
 declare global {
     interface String {
         isNullOrWhiteSpace(): boolean;
+        hasSpecialChars(): boolean;
+        hasNumbers(): boolean;
     }
 }
 
 String.prototype.isNullOrWhiteSpace = function (): boolean {
     return this === null || this === undefined || this.trim() === '';
+}
+
+String.prototype.hasSpecialChars = function (): boolean {
+    const specialCharsRegex: RegExp = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    return specialCharsRegex.test(this.valueOf());
+}
+
+String.prototype.hasNumbers = function (): boolean {
+    return /\d/.test(this.valueOf());
 }
 
 export { }; 
