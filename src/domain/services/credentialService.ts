40baab08bc140448354511cbeb07995603b04294
Ricardo Kromer Cavati <51382474+RicardoKromerCavati@users.noreply.github.com>
2025-06-04 21:48:38 -0300

    Create new command to generate passwords.
    * chore: Generate password based on parameters.
    
    * chore: Create method to generate password for user.
    
    * chore: Create new command to generate new password.
    
    * chore: Add logger to application
    
    * Remove logger.

--- src/domain/services/credentialService.ts
+++ src/domain/services/credentialService.ts
@@ -3,9 +3,12 @@ import { CredentialRepository } from '../../infrastructure/repositories/credenti
 import '../extensions/stringExtensions';
 import '../services/passwordService';
 import { PasswordService } from '../services/passwordService';
+import * as readline from 'readline/promises';
+import { Category } from 'typescript-logging-category-style';
 
 export class CredentialService {
     public static createCredential(credentialName: string, username: string, password: string): void {
+
         try {
             if (password == undefined || password.isNullOrWhiteSpace()) {
                 const passwordLength: number = 12;
@@ -14,7 +17,7 @@ export class CredentialService {
 
             const [result, credential] = Credential.Create(credentialName, username, password);
 
-            if (result && credential != null) {    
+            if (result && credential != null) {
                 new CredentialRepository().createCredential(credential);
 
                 return;
@@ -23,7 +26,7 @@ export class CredentialService {
             console.log('Credential not created');
 
         } catch (error) {
-            console.log(error);
+            console.log(JSON.stringify(error));
         }
     }
 
@@ -32,7 +35,7 @@ export class CredentialService {
             const foundCredentials = await new CredentialRepository().getCredentialNames();
 
             if (foundCredentials.length <= 0) {
-                console.log('no credentials found');
+                console.log('There are no credentials available');
                 return;
             }
 
@@ -41,11 +44,11 @@ export class CredentialService {
 
                 let credential = { Id: element.id, CredentialName: element.credentialName, Username: element.username };
 
-                console.log(credential);
+                console.log(JSON.stringify(credential));
             }
 
         } catch (error) {
-            console.log(error);
+            console.log(JSON.stringify(error));
         }
     }
 
@@ -64,7 +67,82 @@ export class CredentialService {
             console.log('Credential password copied to clipboard');
 
         } catch (error) {
-            console.log(error);
+            console.log(JSON.stringify(error));
         }
+    }
+
+    public static async generatePassword() {
+        const { stdin: input, stdout: output } = require('node:process');
+
+        var rl = readline.createInterface({ input, output });
+
+        console.log('Generate new passowrd!');
+
+        var length = 0;
+
+        while (length == 0) {
+            var answer = await rl.question('Please choose the length from 8 to 128: ');
+
+            const convertedLenght = Number(answer);
+
+            if (isNaN(convertedLenght)) {
+                console.log('Please write a numeric value!');
+                continue;
+            }
+
+            length = convertedLenght;
+
+            if (length < 8 || length > 128) {
+                length = 0;
+                console.log('Please write a numeric value from 8 to 128!');
+                continue;
+            }
+
+            break;
+        }
+
+        var useNumbers = await this.askYesOrNoQuestion(rl, 'Would you like to include numbers? [y/n]: ');
+        var useSpecialChars = await this.askYesOrNoQuestion(rl, 'Would you like to include special characters? [y/n]: ');
+        var useUpperCaseChars = await this.askYesOrNoQuestion(rl, 'Would you like to include upper case characters? [y/n]: ');
+
+        rl.close();
+
+        const generatedPasword = PasswordService.createPassword(length, useNumbers, useSpecialChars, useUpperCaseChars);
+
+        const clipboardy = (await import("clipboardy")).default
+
+        clipboardy.write(generatedPasword);
+
+        console.log('Password was copied to your clipboard');
+    }
+
+    private static async askYesOrNoQuestion(rl: readline.Interface, question: string): Promise<boolean> {
+
+        var tempAnswer: string = '';
+
+        var result = false;
+
+        while (tempAnswer.isNullOrWhiteSpace()) {
+            var answer = await rl.question(question);
+
+            switch (answer) {
+                case 'y':
+                case 'Y':
+                    result = true;
+                    tempAnswer = answer;
+                    break;
+                case 'n':
+                case 'n':
+                    result = false;
+                    tempAnswer = answer;
+                    break;
+                default:
+                    tempAnswer = '';
+                    console.log('Invalid answer, please use \'y\' or \'n\'');
+                    break;
+            }
+        }
+
+        return result;
     }
 }
