fa2ffd2dc8a05fbd1e37b746a0ace9eb948a2fe8
Ricardo Kromer Cavati <51382474+RicardoKromerCavati@users.noreply.github.com>
2025-08-11 18:40:51 -0300

    Release (#12)
    * ci: Allow manual trigger of release pipeline.
    
    * ci: Include assets in array.

--- release.config.js
+++ release.config.js
@@ -5,10 +5,12 @@ const config = {
         '@semantic-release/release-notes-generator',
         [
             "@semantic-release/github", {
-                "assets": {
-                    "path": "dist.zip",
-                    "label": "Build result (zip)"
-                },
+                "assets": [
+                    {
+                        "path": "dist.zip",
+                        "label": "Build result (zip)"
+                    }
+                ],
                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
             }
         ],
