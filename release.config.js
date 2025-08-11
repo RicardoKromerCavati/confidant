b3ec1a423b03bc3411a75f08ea773645d464b627
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-08-11 17:48:38 -0300

    ci: Configure pipelines to run on the right moment.

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-    branches: ['distribution'],
+    branches: ['main'],
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
