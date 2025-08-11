85dedb5768fce16236deb0f5039de31d53f029bf
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-08-11 17:44:40 -0300

    ci: Create label for dist folder zip.

--- release.config.js
+++ release.config.js
@@ -5,7 +5,10 @@ const config = {
         '@semantic-release/release-notes-generator',
         [
             "@semantic-release/github", {
-                "assets": ["dist.zip"],
+                "assets": {
+                    "path": "dist.zip",
+                    "label": "Build result (zip)"
+                },
                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
             }
         ],
