e30b23437805a9152317f33ffca5ee5bbb142ed8
Ricardo Kromer Cavati <ricardokcavati@gmail.com>
2025-08-12 12:13:01 -0300

    Create release on github and publish after.

--- release.config.js
+++ release.config.js
@@ -4,14 +4,15 @@ const config = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        "@semantic-release/npm",
         [
             "@semantic-release/git",
             {
                 "assets": ["dist/**"],
                 "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
             }
-        ]
+        ],
+        '@semantic-release/github',
+        "@semantic-release/npm"
     ]
 };
 
