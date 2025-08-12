e48b7731d485a0c724d8e324a1c709e64ee7ecce
Ricardo Kromer Cavati <51382474+RicardoKromerCavati@users.noreply.github.com>
2025-08-12 12:24:52 -0300

    Create release on github and publish after. (#21)
    * Create release on github and publish after.
    
    * delete dist.
    
    * remove unnecessary package.
    
    * use correct option in plugin.

--- release.config.js
+++ release.config.js
@@ -4,14 +4,13 @@ const config = {
     plugins: [
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
-        "@semantic-release/npm",
         [
-            "@semantic-release/git",
+            "@semantic-release/github",
             {
-                "assets": ["dist/**"],
-                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+                "successComment": "This ${issue.pull_request ? 'pull request' : 'issue'} is included in version ${nextRelease.version}.\n\nCommits on ${branch}:\n\n${commits}"
             }
-        ]
+        ],
+        "@semantic-release/npm"
     ]
 };
 
